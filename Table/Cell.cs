5b03be68a90e8f119c91dd57ff87728c7ce8cc1b
Irina <irinachkas.96@gmail.com>
2016-05-18 20:40:52 +0500

    added

---
+++ Table/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    class Cell
+    {
+        public readonly int ColumnNumber;
+        public readonly int RowNumber;
+        public readonly double Height;
+        public readonly double Width;
+
+        
+
+        public Cell(int columnNumber, int rowNumber, int height, int width)
+        {
+            this.ColumnNumber = columnNumber;
+            this.RowNumber = rowNumber;
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public void PushData()
+        {
+            
+        }
+        
+    }
+}
---
+++ Table/ITable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    interface ITable<T>
+    {
+        T this[int x, int y] { get; set; }
+        void InsertRow(int y);
+        void InsertColomn(int x);
+        void Resize(int deltaX, int deltaY);
+    }
+}
