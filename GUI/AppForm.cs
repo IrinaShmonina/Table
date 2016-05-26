446b0345337dd00f134a95e9ea2edebac949c495
predatorfromekb <stepanus97@yandex.ru>
2016-05-26 20:49:06 +0500

    добавлен новый проект gui

---
+++ GUI/AppForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    class AppForm : Form
+    {
+        public AppForm()
+        {
+            this.Text = "Электронная Таблица";
+            this.Size = new Size(500, 500);
+            //this.ClientSize = new Size(300, 300);
+
+        }
+    }
+}
